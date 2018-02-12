aeb1a95632f1a53a413e77543f57991767045087
takonomura <takonomura@users.noreply.github.com>
2018-02-12 15:07:11 +0900

    Implement exporter

--- main.go
+++ main.go
@@ -1,36 +1,70 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gobot.io/x/gobot/drivers/i2c"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
+var (
+	tempDesc     = prometheus.NewDesc("bme280_temperature_celsius", "Temperature in celsius degree", nil, nil)
+	pressDesc    = prometheus.NewDesc("bme280_pressure_hpa", "Barometric pressure in hPa", nil, nil)
+	humidityDesc = prometheus.NewDesc("bme280_humidity", "Humidity in percentage of relative humidity", nil, nil)
+)
+
+type collector struct {
+	*i2c.BME280Driver
+}
+
+func (c collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- tempDesc
+	ch <- pressDesc
+	ch <- humidityDesc
+}
+
+func (c collector) Collect(ch chan<- prometheus.Metric) {
+	temp, err := c.Temperature()
+	if err != nil {
+		log.Printf("getting temp: %s", err)
+	}
+	ch <- prometheus.MustNewConstMetric(tempDesc, prometheus.GaugeValue, float64(temp))
+
+	press, err := c.Pressure()
+	if err != nil {
+		log.Printf("getting press: %s", err)
+	}
+	ch <- prometheus.MustNewConstMetric(pressDesc, prometheus.GaugeValue, float64(press)/100)
+
+	humidity, err := c.Humidity()
+	if err != nil {
+		log.Printf("getting humidity: %s", err)
+	}
+	ch <- prometheus.MustNewConstMetric(humidityDesc, prometheus.GaugeValue, float64(humidity))
+}
+
 func main() {
-	r := raspi.NewAdaptor()
-	d := i2c.NewBME280Driver(r, i2c.WithBus(1), i2c.WithAddress(0x76))
+	adapter := raspi.NewAdaptor()
+	d := i2c.NewBME280Driver(adapter, i2c.WithBus(1), i2c.WithAddress(0x76))
 
 	if err := d.Start(); err != nil {
 		log.Fatalf("starting driver: %s", err)
 	}
+	log.Print("Connected to BME280")
 
-	temp, err := d.Temperature()
-	if err != nil {
-		log.Fatalf("getting temp: %s", err)
+	r := prometheus.NewRegistry()
+	c := &collector{d}
+	r.MustRegister(c)
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	s := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
-	fmt.Printf("Temperature: %.2f 'C\n", temp)
 
-	press, err := d.Pressure()
-	if err != nil {
-		log.Fatalf("getting press: %s", err)
-	}
-	fmt.Printf("Pressure: %.2f hPa\n", press/100)
-
-	humidity, err := d.Humidity()
-	if err != nil {
-		log.Fatalf("getting humidity: %s", err)
-	}
-	fmt.Printf("Humidity: %.2f %%\n", humidity)
+	log.Print("Starting server...")
+	log.Fatal(s.ListenAndServe())
 }
